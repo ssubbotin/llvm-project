fa0b427925f0c57e03fc9406cd327005f707cd39
Jonathan Thackray <jonathan.thackray@arm.com>
2026-02-04 13:52:59 +0000

    fixup!
    Adjust code

--- llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
+++ llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
@@ -20622,7 +20622,7 @@ tryToReplaceScalarFPConversionWithSVE(SDNode *N, SelectionDAG &DAG,
   if (DCI.isBeforeLegalizeOps())
     return SDValue();
 
-  if (Subtarget->isStreaming() && Subtarget->hasFPRCVT())
+  if (Subtarget->hasFPRCVT())
     return SDValue();
 
   if (!Subtarget->isSVEorStreamingSVEAvailable() ||
